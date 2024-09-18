ac20fd3012f179fc6b5ad03d64da6802719baa1b
Hieu <nguyentrunghieu170221@gmail.com>
2024-09-18 23:41:55 +0700

    add DuyetFile

---
+++ DuyetFile/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Duyet_Picture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.lbThumbnails = new System.Windows.Forms.ListBox();
+            this.pbMainImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(-3, -2);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(120, 36);
+            this.btnOpenFolder.TabIndex = 0;
+            this.btnOpenFolder.Text = "Mở thư mục";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // lbThumbnails
+            // 
+            this.lbThumbnails.FormattingEnabled = true;
+            this.lbThumbnails.ItemHeight = 16;
+            this.lbThumbnails.Location = new System.Drawing.Point(-3, 51);
+            this.lbThumbnails.Name = "lbThumbnails";
+            this.lbThumbnails.Size = new System.Drawing.Size(120, 388);
+            this.lbThumbnails.TabIndex = 1;
+            this.lbThumbnails.SelectedIndexChanged += new System.EventHandler(this.lbThumbnails_SelectedIndexChanged);
+            // 
+            // pbMainImage
+            // 
+            this.pbMainImage.Location = new System.Drawing.Point(123, 51);
+            this.pbMainImage.Name = "pbMainImage";
+            this.pbMainImage.Size = new System.Drawing.Size(665, 388);
+            this.pbMainImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMainImage.TabIndex = 2;
+            this.pbMainImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbMainImage);
+            this.Controls.Add(this.lbThumbnails);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFolder;
+        private System.Windows.Forms.ListBox lbThumbnails;
+        private System.Windows.Forms.PictureBox pbMainImage;
+    }
+}
+
---
+++ DuyetFile/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Duyet_Picture
+{
+    public partial class Form1 : Form
+    {
+        private List<string> _imageFiles;
+        private int _currentIndex;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _imageFiles = new List<string>();
+            _currentIndex = -1;
+
+            // Gán các sự kiện
+            this.KeyDown += Form1_KeyDown;
+            lbThumbnails.SelectedIndexChanged += lbThumbnails_SelectedIndexChanged;
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    LoadImages(folderDialog.SelectedPath);
+                }
+            }
+        }
+
+        private void LoadImages(string path)
+        {
+            try
+            {
+                _imageFiles = Directory.GetFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+                    .Where(s => s.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) ||
+                                s.EndsWith(".jpeg", StringComparison.OrdinalIgnoreCase) ||
+                                s.EndsWith(".png", StringComparison.OrdinalIgnoreCase) ||
+                                s.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+
+                lbThumbnails.Items.Clear();
+                foreach (var file in _imageFiles)
+                {
+                    lbThumbnails.Items.Add(Path.GetFileName(file));
+                }
+
+                if (_imageFiles.Count > 0)
+                {
+                    _currentIndex = 0;
+                    ShowImage(_imageFiles[_currentIndex]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Đã xảy ra lỗi khi tải ảnh: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ShowImage(string filePath)
+        {
+            try
+            {
+                pbMainImage.Image?.Dispose(); // Giải phóng ảnh trước đó
+                pbMainImage.Image = Image.FromFile(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Đã xảy ra lỗi khi hiển thị ảnh: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lbThumbnails_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbThumbnails.SelectedIndex >= 0)
+            {
+                _currentIndex = lbThumbnails.SelectedIndex;
+                ShowImage(_imageFiles[_currentIndex]);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (_imageFiles.Count == 0) return;
+
+            if (e.KeyCode == Keys.Right)
+            {
+                _currentIndex = (_currentIndex + 1) % _imageFiles.Count;
+                lbThumbnails.SelectedIndex = _currentIndex;
+                ShowImage(_imageFiles[_currentIndex]);
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                _currentIndex = (_currentIndex - 1 + _imageFiles.Count) % _imageFiles.Count;
+                lbThumbnails.SelectedIndex = _currentIndex;
+                ShowImage(_imageFiles[_currentIndex]);
+            }
+        }
+
+    }
+}
