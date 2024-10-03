268724142b6686841ea34ee8f862a080dae0f75d
Hieu <nguyentrunghieu170221@gmail.com>
2024-10-03 08:43:06 +0700

    thêm bài lab2

---
+++ Lab02/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lab02_Bai01 bai1Form = new Lab02_Bai01();
+            bai1Form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Lab02_Bai02 Bai2Form = new Lab02_Bai02();
+            Bai2Form.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Lab02_Bai03 bai3Form = new Lab02_Bai03();
+            bai3Form.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Lab02_Bai04 Bai4Form = new Lab02_Bai04();
+            Bai4Form.Show();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Lab02_Bai05 bai5Form = new Lab02_Bai05();
+            bai5Form.Show();
+        }
+
+    }
+}
