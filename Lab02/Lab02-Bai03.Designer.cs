0faf9848d53ff6e56a77323d2431740ecc3ff3c0
Hieu <nguyentrunghieu170221@gmail.com>
2024-10-10 10:41:03 +0700

    thêm lab02-bai03

--- Lab02/Lab02-Bai03.Designer.cs
+++ Lab02/Lab02-Bai03.Designer.cs
@@ -28,58 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnTinhtoan = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnChonFile = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnTinhtoan
+            // btnChonFile
             // 
-            this.btnTinhtoan.Location = new System.Drawing.Point(410, 146);
-            this.btnTinhtoan.Name = "btnTinhtoan";
-            this.btnTinhtoan.Size = new System.Drawing.Size(135, 50);
-            this.btnTinhtoan.TabIndex = 0;
-            this.btnTinhtoan.Text = "Tính toán";
-            this.btnTinhtoan.UseVisualStyleBackColor = true;
+            this.btnChonFile.Location = new System.Drawing.Point(56, 25);
+            this.btnChonFile.Name = "btnChonFile";
+            this.btnChonFile.Size = new System.Drawing.Size(96, 38);
+            this.btnChonFile.TabIndex = 0;
+            this.btnChonFile.Text = "Chọn File";
+            this.btnChonFile.UseVisualStyleBackColor = true;
+            this.btnChonFile.Click += new System.EventHandler(this.btnChonFile_Click);
             // 
-            // textBox1
+            // txtInput
             // 
-            this.textBox1.Location = new System.Drawing.Point(243, 57);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(493, 35);
-            this.textBox1.TabIndex = 1;
+            this.txtInput.Location = new System.Drawing.Point(56, 141);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(265, 258);
+            this.txtInput.TabIndex = 1;
             // 
-            // textBox2
+            // txtOutput
             // 
-            this.textBox2.Location = new System.Drawing.Point(243, 245);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(493, 171);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.txtOutput.Location = new System.Drawing.Point(478, 141);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(265, 258);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(362, 239);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(75, 48);
+            this.btnTinh.TabIndex = 3;
+            this.btnTinh.Text = "Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(35, 57);
+            this.label1.Location = new System.Drawing.Point(53, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(184, 36);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "File cần đọc ";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "INPUT";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(35, 245);
+            this.label2.Location = new System.Drawing.Point(475, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 36);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Output";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "OUTPUT";
             // 
             // Lab02_Bai03
             // 
@@ -88,9 +97,10 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnTinhtoan);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnChonFile);
             this.Name = "Lab02_Bai03";
             this.Text = "Lab02_Bai03";
             this.ResumeLayout(false);
@@ -100,9 +110,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnTinhtoan;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnChonFile;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnTinh;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
     }
--- Lab02/Lab02-Bai03.cs
+++ Lab02/Lab02-Bai03.cs
@@ -1,25 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using NCalc; // Thêm thư viện NCalc vào giúp thực hiện các thao tác tính toán phức tạp hơn 
 
 namespace Lab02
 {
-    public partial class Lab02_Bai03 : System.Windows.Forms.Form
+    public partial class Lab02_Bai03 : Form
     {
+        private string duongDanFile = "";// Biến lưu đường dẫn file được chọn
         public Lab02_Bai03()
         {
             InitializeComponent();
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void btnChonFile_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog hopThoaiMoFile = new OpenFileDialog();
+            hopThoaiMoFile.Filter = "File Văn Bản|*.txt|Tất Cả Các File|*.*";
+            if (hopThoaiMoFile.ShowDialog() == DialogResult.OK)
+            {
+                duongDanFile = hopThoaiMoFile.FileName;
+                // Sử dụng FileStream và StreamReader để đọc file
+                using (FileStream fileStream = new FileStream(duongDanFile, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader docFile = new StreamReader(fileStream))
+                    {
+                        txtInput.Text = docFile.ReadToEnd();
+                    }
+                }
+            }
         }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(duongDanFile))
+            {
+                MessageBox.Show("Vui lòng chọn file !");
+                return;
+            }
+
+            // Đọc file từng dòng và lưu vào mảng
+            string[] cacdong;
+            using (FileStream fileStream = new FileStream(duongDanFile, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader docFile = new StreamReader(fileStream))
+                {
+                    var noiDungFile = docFile.ReadToEnd();
+                    cacdong = noiDungFile.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                }
+            }
+
+            // Tạo chuỗi để lưu kết quả tính toán
+            string chuoiKQ = "";
+
+            // Thực hiện tính toán cho từng dòng
+            foreach (string dong in cacdong)
+            {
+                try
+                {
+                    // Loại bỏ các khoảng trắng không cần thiết
+                    string bieuThuc = dong.Trim();
+
+                    // Kiểm tra xem dòng có trống hay không
+                    if (string.IsNullOrWhiteSpace(bieuThuc))
+                    {
+                        continue; // Bỏ qua các dòng trống
+                    }
+
+                    // Thay dấu ',' bằng dấu '.' để đảm bảo tính đúng số thập phân
+                    bieuThuc = bieuThuc.Replace(",", ".");
+
+                    // Sử dụng NCalc để tính toán giá trị biểu thức
+                    Expression expression = new Expression(bieuThuc);
+
+                    // Tính kết quả của biểu thức
+                    var ketQua = expression.Evaluate();
+
+                    // Định dạng kết quả và thêm vào chuỗi kết quả
+                    chuoiKQ += bieuThuc + " = " + ketQua + Environment.NewLine;
+                }
+                catch (Exception ex)
+                {
+                    // Nếu có lỗi trong quá trình tính toán, hiển thị thông báo lỗi
+                    chuoiKQ += dong + " = Lỗi: " + ex.Message + Environment.NewLine;
+                }
+            }
+
+            // Hiển thị kết quả lên txtOutput
+            txtOutput.Text = chuoiKQ;
+
+            // Ghi kết quả vào file
+            string thuMucKetQua = @"C:\path\to\output";
+            string ketQuaFile = Path.Combine(thuMucKetQua, "KetQua.txt");
+
+            if (!Directory.Exists(thuMucKetQua))
+            {
+                Directory.CreateDirectory(thuMucKetQua);
+            }
+
+            try
+            {
+                using (FileStream fileStream = new FileStream(ketQuaFile, FileMode.Create, FileAccess.Write))
+                {
+                    using (StreamWriter ghiFile = new StreamWriter(fileStream))
+                    {
+                        ghiFile.Write(chuoiKQ); // Ghi kết quả vào file
+                    }
+                }
+
+                MessageBox.Show("Kết quả đã được ghi vào file thành công!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi ghi file: " + ex.Message);
+            }
+        }
+
     }
 }
