7d2dc9328097069f89e704e4959274850187b660
Hieu <nguyentrunghieu170221@gmail.com>
2024-10-31 21:59:46 +0700

    add Lab04

---
+++ Lab/Lab04/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lab04_Bai123 bai123Form = new Lab04_Bai123();
+            bai123Form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Lab04_Bai45 bai45Form = new Lab04_Bai45();
+            bai45Form.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Lab04_Bai06 bai06Forn = new Lab04_Bai06();
+            bai06Forn.Show();
+        }
+    }
+}
