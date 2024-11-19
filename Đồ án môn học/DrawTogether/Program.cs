e3f809e207c139232f6a49c2ea029cefe1878708
AnLikeCode <pnhoaian270205@gmail.com>
2024-11-19 22:32:01 +0700

    Hoàn thành việc giao tiếp giữa Client Server

--- Đồ án môn học/DrawTogether/Program.cs
+++ Đồ án môn học/DrawTogether/Program.cs
@@ -16,7 +16,7 @@ namespace DrawTogether
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DrawTogether());
+            Application.Run(new HOME());
         }
     }
 }
